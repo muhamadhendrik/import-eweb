9a44612e378ac24f0ccf7c68e7763e4b85bd3b7b
Muhamad Hendrik <muhammadhendrik59@gmail.com>
2025-01-23 20:23:01 +0700

    maatwebsite excel

--- routes/web.php
+++ routes/web.php
@@ -4,21 +4,16 @@ use App\Http\Controllers\Acl\RoleController;
 use App\Http\Controllers\Acl\UserController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Back\ImportEwebController;
-use App\Http\Controllers\Customer\CustomerController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\Master\ProductController;
-use App\Http\Controllers\Master\QrcodeController;
-use App\Http\Controllers\Transaction\CustomerController as TransactionCustomerController;
 use Illuminate\Support\Facades\Route;
-use SimpleSoftwareIO\QrCode\Facades\QrCode;
 
 Route::get('backoffice', function () {
     return redirect(route('dashboard'));
 });
 
 // Override halaman login
-Route::get('admin-orivetama', [LoginController::class, 'showLoginForm'])->name('login');
-Route::post('admin-orivetama', [LoginController::class, 'login']);
+Route::get('admin-aplikasi', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('admin-aplikasi', [LoginController::class, 'login']);
 
 // Logout
 Route::post('logout', [LoginController::class, 'logout'])->name('logout');
